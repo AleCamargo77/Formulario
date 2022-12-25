17790cf57c6810bc28aa3f402e6affedd7468864
Alessandro Camargo <acamargodias@terra.com.br>
2022-12-25 14:09:43 -0300

    valida password

--- main.js
+++ main.js
@@ -1,25 +1,35 @@
-function validateEmail() {
+function validateEmail(email) {
   //   e.preventdefault();
-  const email = document.getElementById("email").value;
-  const emailIsValid = email.match(
+  const emailValue = document.getElementById("email").value;
+  const emailIsValid = emailValue.match(
     /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
   );
 
-  if (emailIsValid) {
-    console.log(email);
-  } else {
-    throw new Error("Email invalid");
-  }
+  console.log(emailValue);
 }
 
 try {
-  if (emailIsValid) {
-    console.log(email);
+  const isValidateEmail = validateEmail(email);
+  if (!isValidateEmail) {
+    throw new Error("An error occurred");
   }
 } catch (error) {
-  console.log("An error ocurred");
+  console.log(error.message);
 }
 
 const play = document.getElementById("play");
-console.log(play);
-play.addEventListener("click", validateEmail);
+// console.log(play);
+play.addEventListener("click", validateEmail, validatePassword);
+
+function validatePassword() {
+  const passwordValue = document.getElementById("password").value;
+  const paswordIsValid = passwordValue.match(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
+  );
+
+  if (paswordIsValid) {
+    console.log(passwordValue);
+  } else {
+    throw new Error("Password invalid");
+  }
+}

3c1a1cf0284c7f7a8cf452f868fc066aa8cdb196
Alessandro Camargo <acamargodias@terra.com.br>
2022-12-26 08:34:32 -0300

    validateUser

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
+function validateUsername(name) {
+  const username = userInputs.name.value;
+  if (username === "") {
+    const err = new Error("Nome de usuário inválido");
+    err.input = "name";
+    throw err;
+  }
+}
+
 function validateEmail(email) {
   if (!email.match(/^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i)) {
     const err = new Error("Email inválido");
-    err.input = email;
+    err.input = "email";
     throw err;
   }
+}
+
+function resetFormStyles() {
+  Object.entries(userInputs).forEach(([key, value]) => {
+    value.classList.remove("success", "error");
+  });
 }
 
 const userInputs = {
@@ -12,24 +27,25 @@ const userInputs = {
   password: document.getElementById("password"),
 };
 
-console.log(userInputs.email);
 const form = document.getElementById("form");
-const smal = document.querySelector("small");
 
-console.log(smal);
 form.addEventListener("submit", (ev) => {
   ev.preventDefault();
 
   try {
     userInputs.name.classList.add("success");
     validateEmail(userInputs.email.value);
+    validateUsername(userInputs.name.value);
+    userInputs.name.classList.add("success");
     userInputs.email.classList.add("success");
     console.log(userInputs.email.value);
   } catch (err) {
-    userInputs.email.classList.add("error");
-    document.querySelector(`#email-error`).innerHTML = "Email inválido";
+    userInputs[err.input].classList.add("error");
+
+    document.querySelector(`#${err.input}-error`).textContent = err.message;
   }
 });
+
 // function validatePassword() {
 //   const passwordValue = document.getElementById("password").value;
 //   const paswordIsValid = passwordValue.match(

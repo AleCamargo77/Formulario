82a06bd9db1968ef5ced97a6e36a209906c8e044
Alessandro Camargo <acamargodias@terra.com.br>
2022-12-27 08:27:11 -0300

    form

--- main.js
+++ main.js
@@ -15,10 +15,41 @@ function validateEmail(email) {
   }
 }
 
-function resetFormStyles() {
-  Object.entries(userInputs).forEach(([key, value]) => {
+function validatePassword(password) {
+  if (
+    password.length < 8 ||
+    !password.match(/[a-z]/) ||
+    !password.match(/[A-Z]/) ||
+    !password.match(/\d/) ||
+    !password.match(/[^a-zA-Z\s0-9]/)
+  ) {
+    const err = new Error("Senha inválida");
+    err.input = "password";
+    throw err;
+  }
+  const passwordValue = document.getElementById("password").value;
+  const paswordIsValid = passwordValue.match(
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
+  );
+
+  if (paswordIsValid) {
+    console.log(passwordValue);
+  } else {
+    throw new Error("Password inválido");
+  }
+}
+
+function resetFormStyles(inputs) {
+  Object.entries(inputs).forEach(([key, value]) => {
     value.classList.remove("success", "error");
+    document.querySelector(`#${key}-error`).textContent = "";
   });
+  const logoName = document.getElementById("nameSuc");
+  logoName.style.visibility = "hidden";
+  const logoEmail = document.getElementById("emailSuc");
+  logoEmail.style.visibility = "hidden";
+  const logoPass = document.getElementById("passSuc");
+  logoPass.style.visibility = "hidden";
 }
 
 const userInputs = {
@@ -31,30 +62,28 @@ const form = document.getElementById("form");
 
 form.addEventListener("submit", (ev) => {
   ev.preventDefault();
-
+  resetFormStyles(userInputs);
   try {
-    userInputs.name.classList.add("success");
-    validateEmail(userInputs.email.value);
     validateUsername(userInputs.name.value);
     userInputs.name.classList.add("success");
+    const logoName = document.getElementById("nameSuc");
+    logoName.style.visibility = "visible";
+
+    validateEmail(userInputs.email.value);
     userInputs.email.classList.add("success");
+    const logoEmail = document.getElementById("emailSuc");
+    logoEmail.style.visibility = "visible";
+
+    validatePassword(userInputs.password.value);
+    userInputs.password.classList.add("success");
+    const logoPass = document.getElementById("passSuc");
+    logoPass.style.visibility = "visible";
+
     console.log(userInputs.email.value);
   } catch (err) {
     userInputs[err.input].classList.add("error");
-
     document.querySelector(`#${err.input}-error`).textContent = err.message;
+    const nameErr = document.querySelector(`#${err.input}-logoError`);
+    console.log(nameErr);
   }
 });
-
-// function validatePassword() {
-//   const passwordValue = document.getElementById("password").value;
-//   const paswordIsValid = passwordValue.match(
-//     /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/
-//   );
-
-//   if (paswordIsValid) {
-//     console.log(passwordValue);
-//   } else {
-//     throw new Error("Password invalid");
-//   }
-// }
